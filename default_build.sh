cbea46da934f6fad75451e2f9c9a996bd646ec30
xenius9 <xenius9@yandex.ru>
2016-03-21 15:45:55 +0300

    Switch to 4.6, fix build scripts

--- default_build.sh
+++ default_build.sh
@@ -8,7 +8,7 @@
 make b1w-open_tw-perf_defconfig
 
 # build zImage
-make ARCH=arm CROSS_COMPILE=toolchain/arm-eabi-4.8/bin/arm-eabi- zImage -j9
+make ARCH=arm CROSS_COMPILE=toolchain/arm-eabi-4.6/bin/arm-eabi- zImage -j9
 
 cp arch/arm/boot/zImage work/kernel/out/
 cp ramdisk/ramdisk.cpio.lz4 work/kernel/out/
